b27fe8726c52e3c757019086af14a43cee556e71
jjmirandaa86 <jjmirandaa86@gmail.com>
2024-10-10 23:56:20 +1100

    modify hour and delete comands package.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "471c5f3f1f195501"; }
+/******/ 		__webpack_require__.h = function() { return "38fdfeb97cd1697d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/Components/General/Footer.js
+++ src/Components/General/Footer.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Navbar, Row, Col } from "react-bootstrap";
 import FooterDeveloperBy from "../General/FooterDeveloperBy";
+import moment from "moment";
 
 const Footer = () => {
+	const [date, setDate] = useState(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+	setInterval(() => {
+		setDate(moment().format("MMMM Do YYYY, h:mm:ss a"));
+	}, 1000);
+
 	return (
 		<div>
 			<Container>
-				<Navbar fixed="bottom" bg="dark" data-bs-theme="dark">
-					<FooterDeveloperBy />
+				<Navbar fixed="bottom" bg="light" data-bs-theme="light">
+					<div className="w-100 p-3 float-left">
+						<FooterDeveloperBy />
+					</div>
+					<div className="w-100 p-3">{date}</div>
 				</Navbar>
 			</Container>
 		</div>
@@ -16,3 +26,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
+//<div className="w-100 p-3 float-right">{date}</div>
