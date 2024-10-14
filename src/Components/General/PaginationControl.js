9b61e1ffaf2b44bd1b823535cab4b735d5678180
jjmirandaa86 <jjmirandaa86@gmail.com>
2024-10-14 16:59:30 +1100

    footer pagination control

--- src/Components/General/PaginationControl.js
+++ src/Components/General/PaginationControl.js
@@ -3,10 +3,13 @@ import { Pagination } from "react-bootstrap";
 
 const PaginationControl = ({ data }) => {
 	return (
-		<div className="p-3">
-			<h6>Showing: 1 to 10</h6>
+		<div className="p-2">
+			<div style={{ fontSize: "12px" }} className="p-1">
+				Showing: 1 to 10
+			</div>
+			<div className="p-1"></div>
 			<div className="d-flex justify-content-center">
-				<Pagination>
+				<Pagination size="sm">
 					<Pagination.First />
 					<Pagination.Prev />
 					<Pagination.Item active>{1}</Pagination.Item>
